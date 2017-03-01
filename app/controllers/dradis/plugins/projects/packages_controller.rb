ada34cebbcde217cd8b300c1b736631b50d69886
Daniel Martin <etd-github@nomejortu.com>
2017-03-01 09:09:42 +0100

    Use the new :export_options in the controllers

--- app/controllers/dradis/plugins/projects/packages_controller.rb
+++ app/controllers/dradis/plugins/projects/packages_controller.rb
@@ -1,17 +1,13 @@
 module Dradis::Plugins::Projects
   class PackagesController < Dradis::Plugins::Export::BaseController
     def show
-      # these come from Export#create
-      export_manager_hash   = session[:export_manager].with_indifferent_access
-      content_service_class = export_manager_hash[:content_service].constantize
+      filename = Rails.root.join('tmp', 'dradis-export.zip')
 
-      exporter = Dradis::Plugins::Projects::Export::Package.new(
-        content_service: content_service_class.new(plugin: Dradis::Plugins::Projects)
-      )
+      options  = export_options.merge(plugin: Dradis::Plugins::Projects)
+      exporter = Dradis::Plugins::Projects::Export::Package.new(options)
+      template = exporter.export(filename: filename)
 
-      package_file = Rails.root.join('tmp', 'dradis-export.zip')
-      template     = exporter.export(export_manager_hash.merge(filename: package_file))
-      send_file(package_file)
+      send_file(filename)
     end
   end
 end
--- app/controllers/dradis/plugins/projects/templates_controller.rb
+++ app/controllers/dradis/plugins/projects/templates_controller.rb
@@ -1,15 +1,13 @@
 module Dradis::Plugins::Projects
   class TemplatesController < Dradis::Plugins::Export::BaseController
     def show
-      # these come from Export#create
-      export_manager_hash   = session[:export_manager].with_indifferent_access
-      content_service_class = export_manager_hash[:content_service].constantize
+      # this allows us to have different exporters in different editions
+      exporter_class = Rails.application.config.dradis.projects.template_exporter
 
-      exporter = Rails.application.config.dradis.projects.template_exporter.new(
-        content_service: content_service_class.new(plugin: Dradis::Plugins::Projects)
-      )
+      options  = export_options.merge(plugin: Dradis::Plugins::Projects)
+      exporter = exporter_class.new(options)
+      template = exporter.export
 
-      template = exporter.export(export_manager_hash)
       send_data(template, filename: 'dradis-template.xml', type: :xml)
     end
   end
