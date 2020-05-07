2157075fdd615e7377c751a1f60251bc79baa9d9
Aaron Manaloto <aaronpomanaloto@gmail.com>
2020-05-07 20:44:39 +0800

    Add upload v1 template specs about issue states

--- spec/lib/dradis/plugins/projects/upload/v1/template_spec.rb
+++ spec/lib/dradis/plugins/projects/upload/v1/template_spec.rb
@@ -5,18 +5,21 @@ describe Dradis::Plugins::Projects::Upload::V1::Template::Importer do
   let(:project) { create(:project) }
   let(:user) { create(:user) }
   let(:importer_class) { Dradis::Plugins::Projects::Upload::Template }
-  let(:file_path) {
-    File.join(File.dirname(__FILE__), '../../../../../../', 'fixtures', 'files', 'attachments_url.xml')
-  }
+  let(:importer) do
+    importer_class::Importer.new(
+      state: :published,
+      default_user_id: user.id,
+      plugin: importer_class,
+      project_id: project.id
+    )
+  end
 
   context 'uploading a template with attachments url' do
-    it 'converts the urls' do
-      importer = importer_class::Importer.new(
-        default_user_id: user.id,
-        plugin: importer_class,
-        project_id: project.id
-      )
+    let(:file_path) {
+      File.join(File.dirname(__FILE__), '../../../../../../', 'fixtures', 'files', 'attachments_url.xml')
+    }
 
+    it 'converts the urls' do
       importer.import(file: file_path)
 
       p_id = project.id
@@ -28,6 +31,33 @@ describe Dradis::Plugins::Projects::Upload::V1::Template::Importer do
         "!/pro/projects/#{p_id}/nodes/#{n_id}/attachments/hello.jpg!\n\n" +
         "!/projects/#{p_id}/nodes/#{n_id}/attachments/hello.jpg!"
       )
+    end
+  end
+
+  describe 'issue states' do
+
+    context 'template without states' do
+      let(:file_path) {
+        File.join(File.dirname(__FILE__), '../../../../../../', 'fixtures', 'files', 'without_states.xml')
+      }
+
+      it 'uploads the issues with the published state' do
+        importer.import(file: file_path)
+
+        expect(project.issues.first.state).to eq('published')
+      end
+    end
+
+    context 'template with states' do
+      let(:file_path) {
+        File.join(File.dirname(__FILE__), '../../../../../../', 'fixtures', 'files', 'with_states.xml')
+      }
+
+      it 'uploads the issues with the states' do
+        importer.import(file: file_path)
+
+        expect(project.issues.first.state).to eq('review')
+      end
     end
   end
 end
