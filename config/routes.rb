b8e49fa24e97f27061e6da80d2dcea6854958461
Caitlin <caitlinhenry.ss@gmail.com>
2023-07-06 13:24:04 -0400

    make routes consistent with other export integrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Dradis::Plugins::Projects::Engine.routes.draw do
   resources :projects, only: [] do
-    resource :package,  only: [:create], path: '/export/package'
-    resource :template, only: [:create], path: '/export/template'
+    resource :package,  only: [:create], path: '/export/projects/package'
+    resource :template, only: [:create], path: '/export/projects/template'
   end
 end
